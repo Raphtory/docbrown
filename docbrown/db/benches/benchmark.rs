4a6d29a400864c6d1bff1ed1ee225de45195dd1d
Pedro Rico Pinazo <ricopinazo@gmail.com>
2023-02-16 10:52:05 +0100

    add data module

--- docbrown/db/benches/benchmark.rs
+++ docbrown/db/benches/benchmark.rs
@@ -1,10 +1,14 @@
 use std::collections::hash_map::DefaultHasher;
+use std::error::Error;
 use std::hash::{Hash, Hasher};
+use std::ops::Range;
 use std::path::Path;
-use criterion::{black_box, criterion_group, criterion_main, Criterion, Bencher};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Bencher, BatchSize};
 use csv::StringRecord;
 use docbrown_core::Direction;
-use docbrown_db::GraphDB;
+use docbrown_db::graphdb::GraphDB;
+
+use docbrown_it::data;
 
 fn calculate_hash<T: Hash>(t: &T) -> u64 {
     let mut s = DefaultHasher::new();
@@ -12,49 +16,73 @@ fn calculate_hash<T: Hash>(t: &T) -> u64 {
     s.finish()
 }
 
-fn read_csv(filepath: &str, source: usize, target: usize, time: usize) -> GraphDB {
-    let parse_record = |rec: &StringRecord| {
-        let source_value: String = rec.get(source).unwrap().parse().unwrap();
-        let target_value: String = rec.get(target).unwrap().parse().unwrap();
-        let time_value: i64 = rec.get(time).unwrap().parse().unwrap();
-        (source_value, target_value, time_value)
+fn read_csv(path: &Path, source: usize, target: usize, time: Option<usize>) -> GraphDB {
+    let mut times: Range<i64> = (0..i64::MAX);
+    let mut parse_record = |rec: &StringRecord| {
+        let source_value: String = rec.get(source).ok_or("No source id")?.parse()?;
+        let target_value: String = rec.get(target).ok_or("No target id")?.parse()?;
+        let time_value: i64 = match time {
+            Some(time) => rec.get(time).ok_or("No time value")?.parse()?,
+            None => times.next().ok_or("Max time reached")?
+        };
+        Ok::<(String, String, i64), Box<dyn Error>>((source_value, target_value, time_value))
     };
 
-    let path = Path::new(filepath);
     let mut reader = csv::Reader::from_path(path).unwrap();
     let graph = GraphDB::new(4);
 
     for record in reader.records() {
-        let (source, target, time) = parse_record(&record.unwrap());
+        let record_ok = record.unwrap();
+        let (source, target, time) = parse_record(&record_ok).expect(&format!("Unable to parse record: {:?}", record_ok));
         let src_id = calculate_hash(&source);
         let dst_id = calculate_hash(&target);
-        graph.add_vertex(src_id, time, vec![]);
-        graph.add_vertex(dst_id, time, vec![]);
+        graph.add_vertex(src_id, time, &vec![]);
+        graph.add_vertex(dst_id, time, &vec![]);
         graph.add_edge(src_id, dst_id, time, &vec![]);
     }
 
     graph
 }
 
+// TODO: use different number of partitions using a criterion group
+
+pub fn element_additions(c: &mut Criterion) {
+    let mut graph = GraphDB::new(4);
+    graph.add_vertex(0, 0, &vec![]);
+
+    c.bench_function("existing vertex addition constant time", |b| b.iter(|| graph.add_vertex(1, 0, &vec![])));
+
+    let mut times: Range<i64> = (0..i64::MAX);
+    c.bench_function("existing vertex addition varying time", |b: &mut Bencher| b.iter_batched(|| times.next().unwrap(), |t| graph.add_vertex(0, t, &vec![]), BatchSize::SmallInput));
+
+    let mut indexes: Range<u64> = (0..u64::MAX);
+    c.bench_function("new vertex addition constant time", |b: &mut Bencher| b.iter_batched(|| indexes.next().unwrap(), |vid| graph.add_vertex(vid, 0, &vec![]), BatchSize::SmallInput));
+
+    c.bench_function("existing edge addition constant time", |b| b.iter(|| graph.add_edge(0, 1, 0, &vec![])));
+}
+
 pub fn edges_len(c: &mut Criterion) {
-    let filepath = "resources/test/lotr.csv";
-    let graph = read_csv(filepath, 0, 1, 2);
+    let path = data::lotr().unwrap();
+    let graph = read_csv(&path, 0, 1, Some(2));
 
     c.bench_function("edges len", |b| b.iter(|| graph.edges_len()));
 }
 
-pub fn neighbour_benchmark(c: &mut Criterion) {
-    let filepath = "resources/test/lotr.csv";
-    let graph = read_csv(filepath, 0, 1, 2);
+pub fn degree(c: &mut Criterion) {
+    let path = data::lotr().unwrap();
+    let graph = read_csv(&path, 0, 1, Some(2));
     let vertex = calculate_hash(&"Frodo");
 
-    c.bench_function("neighbour window", |b| b.iter(|| graph.neighbours_window(1000, 2000, vertex, Direction::OUT)));
+    c.bench_function("degree", |b| b.iter(|| graph.degree(vertex, Direction::OUT)));
 }
 
-pub fn ingestion_benchmark(c: &mut Criterion) {
-    let filepath = "resources/test/lotr.csv";
-    c.bench_function("read csv", |b: &mut Bencher| b.iter_with_large_drop(|| read_csv(filepath, 0, 1, 2)));
+pub fn ingestion(c: &mut Criterion) {
+    let lotr = data::lotr().unwrap();
+    c.bench_function("load lotr.csv", |b: &mut Bencher| b.iter_with_large_drop(|| read_csv(&lotr, 0, 1, Some(2))));
+
+    let twitter = data::twitter().unwrap();
+    c.bench_function("load twitter.csv", |b: &mut Bencher| b.iter_with_large_drop(|| read_csv(&twitter, 0, 1, None)));
 }
 
-criterion_group!(benches, neighbour_benchmark, edges_len, ingestion_benchmark);
+criterion_group!(benches, element_additions, edges_len, degree, ingestion);
 criterion_main!(benches);
---
+++ it/src/data.rs
@@ -0,0 +1,30 @@
+use std::{env, fs};
+use std::path::PathBuf;
+use fetch_data::{fetch, FetchDataError};
+
+// In order to add new files to this module, obtain the hash using bin/hash_for_url.rs
+
+pub fn lotr() -> Result<PathBuf, FetchDataError> {
+    fetch_file(
+        "lotr.csv",
+        "https://raw.githubusercontent.com/Raphtory/Data/main/lotr.csv",
+        "c37083a5018827d06de3b884bea8275301d5ef6137dfae6256b793bffb05d033"
+    )
+}
+
+pub fn twitter() -> Result<PathBuf, FetchDataError> {
+    fetch_file(
+        "twitter.csv",
+        "https://raw.githubusercontent.com/Raphtory/Data/main/snap-twitter.csv",
+        "b9cbdf68086c0c6b1501efa2e5ac6b1b0d9e069ad9215cebeba244e6e623c1bb"
+    )
+}
+
+fn fetch_file(name: &str, url: &str, hash: &str) -> Result<PathBuf, FetchDataError> {
+    let tmp_dir = env::temp_dir();
+    let docbrown_dir = tmp_dir.join("docbrown");
+    fs::create_dir_all(&docbrown_dir).expect("Impossible to create directory");
+    let file = docbrown_dir.join(name);
+    fetch(url, hash, &file)?;
+    Ok(file)
+}
