ecb23e3bcc649ef0480fa53013053878afe5d5de
RachelChan <25484244+rachchan@users.noreply.github.com>
2023-02-20 17:28:08 +0000

    move repeating functions out of iter

--- docbrown/algorithms/benches/algobench.rs
+++ docbrown/algorithms/benches/algobench.rs
@@ -20,45 +20,45 @@ pub fn connected_components_lotr_analysis(c: &mut Criterion) {
     let mut benchmark_group = c.benchmark_group("connected_components");
 
     benchmark_group.bench_function("connected-components-lotr", |b| {
-        b.iter(|| {
-            let g = GraphDB::new(1);
-            let csv_path: PathBuf = [env!("CARGO_MANIFEST_DIR"), "../../resources"]
-                .iter()
-                .collect();
-            //insert file name inside +() e.g. +(lotr.csv)
-            let r = Regex::new(r".+(lotr.csv)").unwrap();
-            let csv_loader = CsvLoader::new(Path::new(&csv_path));
-            csv_loader
-                .set_header(false)
-                .set_delimiter(",")
-                .with_filter(r)
-                .load_into_graph(&g, |lotr: Lotr, graph: &GraphDB| {
-                    let src_id = calculate_hash(&lotr.src_id);
-                    let dst_id = calculate_hash(&lotr.dst_id);
-                    let time = lotr.time;
+        let g = GraphDB::new(1);
+        let csv_path: PathBuf = [env!("CARGO_MANIFEST_DIR"), "../../resource"]
+            .iter()
+            .collect();
+        //insert file name inside +() e.g. +(lotr.csv)
+        let r = Regex::new(r".+(lotr.csv)").unwrap();
+        let csv_loader = CsvLoader::new(Path::new(&csv_path));
+        csv_loader
+            .set_header(true)
+            .set_delimiter(",")
+            .with_filter(r)
+            .load_into_graph(&g, |lotr: Lotr, graph: &GraphDB| {
+                let src_id = calculate_hash(&lotr.src_id);
+                let dst_id = calculate_hash(&lotr.dst_id);
+                let time = lotr.time;
 
-                    graph.add_vertex(
-                        src_id,
-                        time,
-                        &vec![("name".to_string(), Prop::Str("Character".to_string()))],
-                    );
-                    graph.add_vertex(
-                        dst_id,
-                        time,
-                        &vec![("name".to_string(), Prop::Str("Character".to_string()))],
-                    );
-                    graph.add_edge(
-                        src_id,
-                        dst_id,
-                        time,
-                        &vec![(
-                            "name".to_string(),
-                            Prop::Str("Character Co-occurrence".to_string()),
-                        )],
-                    );
-                })
-                .expect("Csv did not parse.");
-            let gv = WindowedView::new(&g, 0..32674);
+                graph.add_vertex(
+                    src_id,
+                    time,
+                    &vec![("name".to_string(), Prop::Str("Character".to_string()))],
+                );
+                graph.add_vertex(
+                    dst_id,
+                    time,
+                    &vec![("name".to_string(), Prop::Str("Character".to_string()))],
+                );
+                graph.add_edge(
+                    src_id,
+                    dst_id,
+                    time,
+                    &vec![(
+                        "name".to_string(),
+                        Prop::Str("Character Co-occurrence".to_string()),
+                    )],
+                );
+            })
+            .expect("Csv did not parse.");
+        let gv = WindowedView::new(&g, 0..32674);
+        b.iter(|| {
             connected_components(&gv).unwrap();
         })
     });
