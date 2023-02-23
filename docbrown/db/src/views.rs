3f2aa302fb3cfb84d1d9ed75e0c012c9eb073f0f
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2023-02-23 12:54:42 +0000

    impl vertices apis on view

--- docbrown/db/src/views.rs
+++ docbrown/db/src/views.rs
@@ -1,21 +1,7 @@
-// pub struct Vertex {
-//     id: u64,
-//     time: i64,
-// }
-
-// pub trait TemporalGraphView {
-//     fn vertices(&self) -> Box<dyn Iterator<Item = Vertex> + Send>;
-// }
-
-// struct WindowedGraph<G: TemporalGraphView> {
-//     graph: G,
-//     window: (i64, i64), // fancy window stuff could be here
-// }
-
 use crate::graphdb::GraphDB;
 use docbrown_core::tpartition::TVertex;
 
-use std::{ops::Range, sync::Arc};
+use std::sync::Arc;
 
 pub struct WindowedGraph {
     gdb: Arc<GraphDB>,
@@ -44,12 +30,35 @@ impl WindowedGraph {
         self.gdb.vertices_window(self.t_start, self.t_end)
     }
 }
-
 #[cfg(test)]
 mod views_test {
     use crate::graphdb::GraphDB;
 
     use super::WindowedGraph;
+
+    #[test]
+    fn get_vertex_ids() {
+        let vs = vec![
+            (1, 1, 2),
+            (2, 1, 3),
+            (-1, 2, 1),
+            (0, 1, 1),
+            (7, 3, 2),
+            (1, 1, 1),
+        ];
+
+        let g = GraphDB::new(2);
+
+        for (t, src, dst) in &vs {
+            g.add_edge(*t, *src, *dst, &vec![]);
+        }
+
+        let wg = WindowedGraph::new(g.into(), 0, 7);
+
+        let mut vw = wg.vertex_ids().collect::<Vec<_>>();
+        vw.sort();
+        assert_eq!(vw, vec![1, 2, 3])
+    }
 
     #[test]
     fn get_vertices() {
@@ -68,8 +77,10 @@ mod views_test {
             g.add_edge(*t, *src, *dst, &vec![]);
         }
 
-        let wg = WindowedGraph::new(g.into(), 0, 7);
+        let wg = WindowedGraph::new(g.into(), -1, 1);
 
-        // assert_eq!(wg.ver().collect(), [])
+        let mut vw = wg.vertices().map(|tv| tv.g_id).collect::<Vec<_>>();
+        vw.sort();
+        assert_eq!(vw, vec![1, 2])
     }
 }
