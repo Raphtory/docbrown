ecbcc7bbc8320a4554b5452b8f777c05cd54dbaa
Pedro Rico Pinazo <ricopinazo@gmail.com>
2023-02-24 12:24:22 +0000

    add parameterized benchmarks

--- docbrown/db/benches/base.rs
+++ docbrown/db/benches/base.rs
@@ -1,17 +1,18 @@
-use criterion::{criterion_group, criterion_main, BatchSize, Bencher, Criterion, Throughput};
-use csv::StringRecord;
-use csv_sniffer::Type;
-use docbrown_db::data;
-use docbrown_db::graphdb::GraphDB;
-use rand::distributions::Uniform;
-use rand::Rng;
 use std::collections::hash_map::DefaultHasher;
 use std::error::Error;
 use std::fs::File;
 use std::hash::{Hash, Hasher};
 use std::ops::Range;
 use std::path::Path;
-use std::time::Duration;
+use common::{run_ingestion_benchmarks, run_analysis_benchmarks};
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
+use csv::StringRecord;
+use csv_sniffer::Type;
+use docbrown_db::data;
+use docbrown_db::graphdb::GraphDB;
+use crate::common::bootstrap_graph;
+
+mod common;
 
 fn hash<T: Hash>(t: &T) -> u64 {
     let mut s = DefaultHasher::new();
@@ -58,80 +59,22 @@ fn load_csv(graph: &mut GraphDB, path: &Path, source: usize, target: usize, time
     }
 }
 
-pub fn ingestion(c: &mut Criterion) {
-    fn make_index_gen() -> Box<dyn Iterator<Item = u64>> {
-        let mut rng = rand::thread_rng();
-        let range = Uniform::new(u64::MIN, u64::MAX);
-        Box::new(rng.sample_iter(range))
-    }
-    fn make_time_gen() -> Box<dyn Iterator<Item = i64>> {
-        let mut rng = rand::thread_rng();
-        let range = Uniform::new(i64::MIN, i64::MAX);
-        Box::new(rng.sample_iter(range))
-    }
+pub fn base(c: &mut Criterion) {
+    let mut ingestion_group = c.benchmark_group("ingestion");
+    ingestion_group.throughput(Throughput::Elements(1));
+    run_ingestion_benchmarks(&mut ingestion_group, || bootstrap_graph(4, 10_000), None);
+    ingestion_group.finish();
 
-    let mut indexes = make_index_gen();
-    let mut times = make_time_gen();
-    let mut index_sample = || indexes.next().unwrap();
-    let mut time_sample = || times.next().unwrap();
-
-    let mut group = c.benchmark_group("ingestion");
-    group.throughput(Throughput::Elements(1));
-
-    // Creates a graph with 50k random edges -> 100k random vertices
-    fn bootstrap_graph() -> GraphDB {
+    fn lotr_graph() -> GraphDB {
+        let lotr = data::lotr().unwrap();
         let mut graph = GraphDB::new(4);
-        let mut indexes = make_index_gen();
-        let mut times = make_time_gen();
-        for _ in 0..50_000 {
-            let source = indexes.next().unwrap();
-            let target = indexes.next().unwrap();
-            let time = times.next().unwrap();
-            graph.add_edge(source, target, time, &vec![]);
-        }
+        load_csv(&mut graph, &lotr, 0, 1, Some(2));
         graph
-    };
-
-    group.bench_function("existing vertex varying time", |b: &mut Bencher| {
-        b.iter_batched_ref(
-            || (bootstrap_graph(), time_sample()),
-            |(g, t): &mut (GraphDB, i64)| g.add_vertex(0, *t, &vec![]),
-            BatchSize::SmallInput,
-        )
-    });
-    group.bench_function("new vertex constant time", |b: &mut Bencher| {
-        b.iter_batched_ref(
-            || (bootstrap_graph(), index_sample()),
-            |(g, v): &mut (GraphDB, u64)| g.add_vertex(*v, 0, &vec![]),
-            BatchSize::SmallInput,
-        )
-    });
-    group.bench_function("existing edge varying time", |b: &mut Bencher| {
-        b.iter_batched_ref(
-            || (bootstrap_graph(), time_sample()),
-            |(g, t)| g.add_edge(0, 0, *t, &vec![]),
-            BatchSize::SmallInput,
-        )
-    });
-    group.bench_function("new edge constant time", |b: &mut Bencher| {
-        b.iter_batched_ref(
-            || (bootstrap_graph(), index_sample(), index_sample()),
-            |(g, s, d)| g.add_edge(*s, *d, 0, &vec![]),
-            BatchSize::SmallInput,
-        )
-    });
-
-    group.finish();
+    }
+    let mut analysis_group = c.benchmark_group("analysis");
+    run_analysis_benchmarks(&mut analysis_group, lotr_graph, None);
+    analysis_group.finish();
 }
 
-pub fn analysis(c: &mut Criterion) {
-    let mut group = c.benchmark_group("analysis");
-    let lotr = data::lotr().unwrap();
-    let mut graph = GraphDB::new(4);
-    load_csv(&mut graph, &lotr, 0, 1, Some(2));
-    group.bench_function("edges_len", |b| b.iter(|| graph.edges_len()));
-    group.finish();
-}
-
-criterion_group!(benches, ingestion, analysis);
+criterion_group!(benches, base);
 criterion_main!(benches);
