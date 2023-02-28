1facfc5886be8ef8432ffc836cb53fd3fb606972
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2023-02-28 16:09:03 +0000

    init windowed vertex

--- docbrown/db/src/graph_window.rs
+++ docbrown/db/src/graph_window.rs
@@ -6,6 +6,7 @@ use docbrown_core::{
 
 use std::sync::Arc;
 
+#[derive(Debug, Clone)]
 pub struct WindowedGraph {
     graph: Arc<Graph>,
     pub t_start: i64,
@@ -33,8 +34,13 @@ impl WindowedGraph {
         self.graph.vertex_ids_window(self.t_start, self.t_end)
     }
 
-    pub fn vertices(&self) -> Box<dyn Iterator<Item = TVertex> + Send> {
-        self.graph.vertices_window(self.t_start, self.t_end)
+    pub fn vertices(&self) -> Box<dyn Iterator<Item = WindowedVertex> + Send> {
+        let graph_w = self.clone();
+        Box::new(
+            self.graph
+                .vertices_window(self.t_start, self.t_end)
+                .map(move |tv| WindowedVertex::from(tv, Arc::new(graph_w.clone()))),
+        )
     }
 
     pub fn neighbours(&self, v: u64, d: Direction) -> Box<dyn Iterator<Item = TEdge> + Send> {
@@ -42,8 +48,34 @@ impl WindowedGraph {
     }
 }
 
+pub struct WindowedVertex {
+    g_id: u64,
+    graph_w: Arc<WindowedGraph>,
+}
+
+impl WindowedVertex {
+    fn from(value: TVertex, graph_w: Arc<WindowedGraph>) -> Self {
+        Self {
+            g_id: value.g_id,
+            graph_w,
+        }
+    }
+}
+
+impl WindowedVertex {
+    pub fn degree(&self, d: Direction) -> usize {
+        self.graph_w.degree(self.g_id, d)
+    }
+
+    pub fn neighbours(&self, d: Direction) -> Box<dyn Iterator<Item = TEdge> + Send> {
+        self.graph_w.neighbours(self.g_id, d)
+    }
+}
+
 #[cfg(test)]
 mod views_test {
+    use docbrown_core::Direction;
+
     use super::WindowedGraph;
     use crate::graph::Graph;
 
@@ -93,5 +125,30 @@ mod views_test {
         let mut vw = wg.vertices().map(|tv| tv.g_id).collect::<Vec<_>>();
         vw.sort();
         assert_eq!(vw, vec![1, 2])
+    }
+
+    #[test]
+    fn get_windowed_vertices_degree() {
+        let vs = vec![
+            (1, 1, 2),
+            (2, 1, 3),
+            (-1, 2, 1),
+            (0, 1, 1),
+            (7, 3, 2),
+            (1, 1, 1),
+        ];
+
+        let g = Graph::new(2);
+
+        for (t, src, dst) in &vs {
+            g.add_edge(*t, *src, *dst, &vec![]);
+        }
+
+        let wg = WindowedGraph::new(g.into(), -1, 1);
+
+        let vw = wg.vertices();
+        for v in vw {
+            println!("vid = {}, degree = {}", v.g_id, v.degree(Direction::BOTH))
+        }
     }
 }
