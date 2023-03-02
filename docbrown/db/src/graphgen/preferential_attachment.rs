522c1c6bbfca8934ebf7d7144ffca56a2cb439ef
miratepuffin <b.a.steer@qmul.ac.uk>
2023-03-02 10:03:57 +0000

    test

--- docbrown/db/src/graphgen/preferential_attachment.rs
+++ docbrown/db/src/graphgen/preferential_attachment.rs
@@ -1,30 +1,27 @@
-use crate::{graphdb::GraphDB};
+use crate::graph::Graph;
 use rand::prelude::*;
-use rand::distributions::WeightedIndex;
 use docbrown_core::Direction;
 use crate::graphgen::random::random_graph;
 
-pub fn preferential_attachment(graph:GraphDB,nodes:u64) -> GraphDB {
+pub fn preferential_attachment(graph:Graph,nodes:u64) -> Graph {
     let mut rng = rand::thread_rng();
-    let max_id = graph.vertices().max().expect("Could not get max vertex ID of graph");
+    let max_id = graph.vertex_ids().max().expect("Could not get max vertex ID of graph, please provide a starting graph with nodes/edges for this model");
     let range = max_id+1..max_id+nodes+1;
-    println!("{}",range.start);
-    println!("{}",range.end);
-    let mut nodes:Vec<u64> = graph.vertices().collect();
-    let mut weights:Vec<usize> =graph.vertices()
+    let mut nodes:Vec<u64> = graph.vertex_ids().collect();
+    let mut weights:Vec<usize> =graph.vertex_ids()
         .map(|v| graph.degree(v, Direction::BOTH)).collect();
 
     for i in range {
+        let neighbour_1 = rng.gen_range(0..=graph.edges_len());
+        let neighbour_2 = rng.gen_range(0..=graph.edges_len());
+        let neighbour_3 = rng.gen_range(0..=graph.edges_len());
         nodes.push(i);//add the new node
         weights.push(0); // make sure we can't add self loops
-        //recreating this is heavy as all hell - may need to be rethought - goes from 4 secs to 200ms
-        let dist = WeightedIndex::new(&weights).unwrap();
         //TODO do not select the same nodes
-        for j in 0..3{
-            let sample_index = dist.sample(&mut rng);
-            let node = nodes[sample_index];
-            graph.add_edge(i,node,1,&vec![]);
-            weights[sample_index]+=1;
+        for j in weights.len(){
+            let node = nodes[j];
+            graph.add_edge(1,node,i,&vec![]);
+            weights[j]+=1;
         }
         let new_node_pos = weights.len()-1;
         weights[new_node_pos]+=3;//Update the new node to have the correct weight
@@ -40,10 +37,10 @@ mod preferential_attachment_tests {
     use super::*;
     #[test]
     fn graph_size() {
-        let graph = GraphDB::new(2);
+        let graph = Graph::new(2);
         let graph = random_graph(graph,10);
         let graph =preferential_attachment(graph,10000);
-        let mut degree:Vec<usize> =graph.vertices()
+        let mut degree:Vec<usize> =graph.vertex_ids()
             .map(|v| graph.degree(v, Direction::BOTH)).collect();
         degree.sort();
         println!("{:?}",degree);
--- docbrown/db/src/graphgen/random.rs
+++ docbrown/db/src/graphgen/random.rs
@@ -1,12 +1,12 @@
-use crate::{graphdb::GraphDB};
+use crate::graph::Graph;
 
-pub fn random_graph(graph:GraphDB,edges:u64) -> GraphDB {
+pub fn random_graph(graph:Graph,edges:u64) -> Graph {
     use rand::seq::IteratorRandom;
     let mut rng = rand::thread_rng();
     graph.add_edge(1,2,1,&vec![]); //kick things off }
     for i in 2..edges+1 {
-        let prev_node = graph.vertices().choose(&mut rng).unwrap();
-        graph.add_edge(i,prev_node,1,&vec![]);
+        let prev_node = graph.vertex_ids().choose(&mut rng).unwrap();
+        graph.add_edge(1,prev_node,i,&vec![]);
     }
     graph
 }
@@ -16,7 +16,7 @@ mod random_test {
     use super::*;
     #[test]
     fn graph_size() {
-        let graph = GraphDB::new(2);
+        let graph = Graph::new(2);
         let graph =random_graph(graph,10);
         assert_eq!(graph.edges_len(), 10);
     }
