3192963f824ca068d17508e7909aa27c75b874d1
RachelChan <25484244+rachchan@users.noreply.github.com>
2023-03-06 15:13:36 +0000

    add algobench to cargo toml

--- docbrown/db/benches/algobench.rs
+++ docbrown/db/benches/algobench.rs
@@ -3,32 +3,28 @@ use docbrown_db::algorithms::local_clustering_coefficient::local_clustering_coef
 use docbrown_db::algorithms::local_triangle_count::local_triangle_count;
 use docbrown_db::graph::Graph;
 use crate::common::bench;
-use criterion::Throughput;
 
 mod common;
 
 pub fn local_triangle_count_analysis(c: &mut Criterion) {
     let mut group = c.benchmark_group("local_triangle_count");
-    group.warm_up_time(std::time::Duration::from_secs(1));
-    group.sample_size(10);
-    group.throughput(Throughput::Elements(1_000));
-    group.measurement_time(std::time::Duration::from_secs(3));
+
     bench(
         &mut group,
         "local_triangle_count",
         None,
         |b| {
+            let g: Graph = Graph::new(1);
+            let windowed_graph = g.window(0, 5);
+    
+            let vs = vec![(1, 1, 2), (2, 1, 3), (3, 2, 1), (4, 3, 2)];
+    
+            for (t, src, dst) in &vs {
+                g.add_edge(*t, *src, *dst, &vec![]);
+            }
             b.iter(|| {
-                let g: Graph = Graph::new(1);
-                let windowed_graph = g.window(0, 5);
-        
-                let vs = vec![(1, 1, 2), (2, 1, 3), (3, 2, 1), (4, 3, 2)];
-        
-                for (t, src, dst) in &vs {
-                    g.add_edge(*t, *src, *dst, &vec![]);
-                }
                  local_triangle_count(&windowed_graph, 1)
-            });
+            })
         }
     );
 
@@ -38,17 +34,25 @@ pub fn local_triangle_count_analysis(c: &mut Criterion) {
 pub fn local_clustering_coefficient_analysis(c: &mut Criterion) {
     let mut group = c.benchmark_group("local_clustering_coefficient");
 
-    group.bench_function("local_clustering_coefficient", |b| {
-        let g: Graph = Graph::new(1);
-        let windowed_graph = g.window(0, 5);
-        let vs = vec![(1, 1, 2), (2, 1, 3), (3, 2, 1), (4, 3, 2)];
+    bench(
+        &mut group,
+        "local_clustering_coefficient",
+        None,
+        |b| {
+            let g: Graph = Graph::new(1);
+            let windowed_graph = g.window(0, 5);
+    
+            let vs = vec![(1, 1, 2), (2, 1, 3), (3, 2, 1), (4, 3, 2)];
+    
+            for (t, src, dst) in &vs {
+                g.add_edge(*t, *src, *dst, &vec![]);
+            }
 
-        for (t, src, dst) in &vs {
-            g.add_edge(*t, *src, *dst, &vec![]);
+            b.iter(|| {
+                local_clustering_coefficient(&windowed_graph, 1)
+            })
         }
-
-        b.iter(|| local_clustering_coefficient(&windowed_graph, 1))
-    });
+    );
 
     group.finish();
 }
