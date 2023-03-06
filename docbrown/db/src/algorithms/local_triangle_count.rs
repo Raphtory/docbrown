9d89f7428216998b4750f6e9770c3c9cfdb4e66d
RachelChan <25484244+rachchan@users.noreply.github.com>
2023-03-06 16:54:55 +0000

    add unique to neighbours

--- docbrown/db/src/algorithms/local_triangle_count.rs
+++ docbrown/db/src/algorithms/local_triangle_count.rs
@@ -1,40 +1,25 @@
-use std::collections::HashSet;
-use itertools::Itertools;
-use docbrown_core::Direction;
 use crate::graph_window::WindowedGraph;
+use docbrown_core::Direction;
+use itertools::Itertools;
 
 pub fn local_triangle_count(windowed_graph: &WindowedGraph, v: u64) -> u32 {
     let mut number_of_triangles: u32 = 0;
     let vertex = windowed_graph.vertex(v).unwrap();
 
     if windowed_graph.has_vertex(v) && vertex.degree() >= 2 {
-
-        windowed_graph.neighbours_ids(v, Direction::BOTH).unique()
+        windowed_graph
+            .neighbours_ids(v, Direction::BOTH)
+            .unique()
             .combinations(2)
             .for_each(|nb| {
-                dbg!(&nb, &v);
-                if windowed_graph.has_edge(nb[0], nb[1]) || (windowed_graph.has_edge(nb[1], nb[0])) {
+                if windowed_graph.has_edge(nb[0], nb[1]) || (windowed_graph.has_edge(nb[1], nb[0]))
+                {
                     number_of_triangles += 1;
                 }
             })
     }
     number_of_triangles
 }
-
-// pub fn local_triangle_count(graph: &WindowedGraph, v: u64) -> u32 {
-//     let mut number_of_triangles: u32 = 0;
-//     let vertex = graph.vertex(v).unwrap();
-
-//     if graph.has_vertex(v) && vertex.degree() >= 2 {
-//             .combinations(2)
-//             .for_each(|v| {
-//                 if graph.has_edge(v[0], v[1]) || (graph.has_edge(v[1], v[0])) {
-//                     number_of_triangles += 1;
-//                 }
-//             })
-//     }
-//     number_of_triangles / 3
-// }
 
 #[cfg(test)]
 mod triangle_count_tests {
@@ -53,13 +38,11 @@ mod triangle_count_tests {
         }
 
         let windowed_graph = g.window(0, 5);
-        // let expected = vec![(1), (1), (1)];
-        let expected = 1;
+        let expected = vec![(1), (1), (1)];
 
-        let actual = local_triangle_count(&windowed_graph, 1);
-        // let actual = (1..=3)
-        //     .map(|v| local_triangle_count(&windowed_graph, v))
-        //     .collect::<Vec<_>>();
+        let actual = (1..=3)
+            .map(|v| local_triangle_count(&windowed_graph, v))
+            .collect::<Vec<_>>();
 
         assert_eq!(actual, expected);
     }
