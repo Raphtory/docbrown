e6c4cae3cd4efbdd18ef779b9b617e173162fbac
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2023-03-10 10:59:59 +0000

    add to lib.rs

--- raphtory/src/lib.rs
+++ raphtory/src/lib.rs
@@ -8,18 +8,19 @@ use pyo3::prelude::*;
 
 use crate::wrappers::Direction;
 use crate::graph::Graph;
-use crate::algorithms::triangle_count;
-use crate::graph_gen::random_attachment;
-use crate::graph_gen::ba_preferential_attachment;
+use crate::algorithms::{triangle_count, global_reciprocity, local_reciprocity, all_local_reciprocity};
+use crate::graph_gen::{random_attachment, ba_preferential_attachment};
 
 #[pymodule]
 fn raphtory(py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<Direction>()?;
     m.add_class::<Graph>()?;
 
-
     let algorithm_module = PyModule::new(py, "algorithms")?;
     algorithm_module.add_function(wrap_pyfunction!(triangle_count, algorithm_module)?)?;
+    algorithm_module.add_function(wrap_pyfunction!(global_reciprocity, algorithm_module)?)?;
+    algorithm_module.add_function(wrap_pyfunction!(local_reciprocity, algorithm_module)?)?;
+    algorithm_module.add_function(wrap_pyfunction!(all_local_reciprocity, algorithm_module)?)?;
     m.add_submodule(algorithm_module)?;
 
     let graph_gen_module = PyModule::new(py, "graph_gen")?;
