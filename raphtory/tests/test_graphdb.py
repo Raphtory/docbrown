173bd31ea40ae4db3d65105b3f2e39d38b227381
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2023-03-13 17:14:19 +0000

    add save and load tests

--- raphtory/tests/test_graphdb.py
+++ raphtory/tests/test_graphdb.py
@@ -2,7 +2,7 @@ import sys
 from raphtory import Graph
 from raphtory import algorithms
 from raphtory import Perspective
-
+import tempfile
 
 def create_graph(num_shards):
     g = Graph(num_shards)
@@ -322,28 +322,30 @@ def test_perspective_set():
 
 def test_save_load_graph():
     g = create_graph(1)
-    g.save("test_graph")
-
     g.add_vertex(1, 11, {"type": "wallet", "balance": 99.5})
     g.add_vertex(2, 12, {"type": "wallet", "balance": 10.0})
     g.add_vertex(3, 13, {"type": "wallet", "balance": 76})
-
     g.add_edge(4, 11, 12, {"prop1": 1, "prop2": 9.8, "prop3": "test"})
     g.add_edge(5, 12, 13, {"prop1": 1321, "prop2": 9.8, "prop3": "test"})
     g.add_edge(6, 13, 11, {"prop1": 645, "prop2": 9.8, "prop3": "test"})
 
-    view  = g.window(0,10)
+    tmpdirname = tempfile.TemporaryDirectory()
+    g.save_to_file(tmpdirname.name)
 
-    print(g.has_vertex(13))
-    print(view.vertex(13).in_degree())
-    print(view.vertex(13).out_degree())
-    print(view.vertex(13).degree())
+    del(g)
 
+    g = Graph.load_from_file(tmpdirname.name)
 
+    view = g.window(0,10)
+    assert g.has_vertex(13)
+    assert view.vertex(13).in_degree() == 1
+    assert view.vertex(13).out_degree() == 1
+    assert view.vertex(13).degree() == 2
 
-    triangles  = algorithms.local_triangle_count(view,13) # How many triangles is 13 involved in
-    print(triangles)
+    triangles = algorithms.local_triangle_count(view,13) # How many triangles is 13 involved in
+    assert triangles == 1
 
+    v = view.vertex(11)
+    assert v.props() == {'type': [(1, 'wallet')], 'balance': [(1, 99.5)]}
 
-g2 = Graph.load("test_graph")
-    assert g == g2
+    tmpdirname.cleanup()
