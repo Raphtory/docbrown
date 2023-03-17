35d3397ed782e4f2dbc176d09e18db6123ac65c0
Pedro Rico Pinazo <ricopinazo@gmail.com>
2023-03-17 16:12:57 +0000

    replace lazy_vec tests

--- docbrown/core/src/lazy_vec.rs
+++ docbrown/core/src/lazy_vec.rs
@@ -3,7 +3,6 @@ use crate::Prop;
 use serde::{Deserialize, Serialize};
 use std::ops::Range;
 
-
 #[derive(Default, Debug, PartialEq, Serialize, Deserialize)]
 pub(crate) enum LazyVec<A> {
     #[default]
@@ -34,14 +33,6 @@ where
         }
     }
 
-    pub(crate) fn get_mut(&mut self, id: usize) -> Option<&mut A> {
-        match self {
-            LazyVec::LazyVec1(only_id, value) if *only_id == id => Some(value),
-            LazyVec::LazyVecN(vec) => vec.get_mut(id),
-            _ => None,
-        }
-    }
-
     pub(crate) fn get(&self, id: usize) -> Option<&A> {
         match self {
             LazyVec::LazyVec1(only_id, value) if *only_id == id => Some(value),
@@ -50,22 +41,20 @@ where
         }
     }
 
-    // fails if there is already a value set for the given id and it's different from the default
-    pub(crate) fn set(&mut self, id: usize, value: A) -> Result<(), ()> {
+    // fails if there is already a value set for the given id
+    pub(crate) fn set(&mut self, id: usize, value: A) {
         match self {
             LazyVec::Empty => {
                 *self = Self::from(id, value);
-                Ok(())
             }
             LazyVec::LazyVec1(only_id, only_value) => {
                 if *only_id == id {
-                    Err(())
+                    panic!("cannot set value in position '{id}' because it was perviously set");
                 } else {
                     let mut vector = vec![Default::default(); usize::max(id, *only_id) + 1];
                     vector[id] = value;
                     vector[*only_id] = only_value.clone();
                     *self = LazyVec::LazyVecN(vector);
-                    Ok(())
                 }
             }
             LazyVec::LazyVecN(vector) => {
@@ -74,11 +63,18 @@ where
                 }
                 if vector[id] == Default::default() {
                     vector[id] = value;
-                    Ok(())
                 } else {
-                    Err(())
+                    panic!("cannot set value in position '{id}' because it was perviously set");
                 }
             }
+        }
+    }
+
+    fn get_mut(&mut self, id: usize) -> Option<&mut A> {
+        match self {
+            LazyVec::LazyVec1(only_id, value) if *only_id == id => Some(value),
+            LazyVec::LazyVecN(vec) => vec.get_mut(id),
+            _ => None,
         }
     }
 
@@ -88,175 +84,42 @@ where
     {
         match self.get_mut(id) {
             Some(value) => updater(value),
-            None => self.set(id, default).unwrap(),
+            None => self.set(id, default),
         }
     }
 }
 
-// TODO: move all these test to Props ?
+#[cfg(test)]
+mod lazy_vec_tests {
+    use super::*;
 
-// #[cfg(test)]
-// mod tpropvec_tests {
-//     use super::*;
-//
-//     #[test]
-//     fn set_new_prop_for_tpropvec_initialized_as_empty() {
-//         let mut tpropvec = LazyVec::Empty;
-//         let prop_id = 1;
-//         tpropvec.set_temporal(prop_id, 1, &Prop::I32(10));
-//
-//         assert_eq!(
-//             tpropvec.iter_temporal(prop_id).collect::<Vec<_>>(),
-//             vec![(&1, Prop::I32(10))]
-//         );
-//     }
-//
-//     #[test]
-//     fn set_multiple_props() {
-//         let mut tpropvec = LazyVec::from_temporal(1, 1, &Prop::Str("Pometry".into()));
-//         tpropvec.set_temporal(2, 2, &Prop::I32(2022));
-//         tpropvec.set_temporal(3, 3, &Prop::Str("Graph".into()));
-//
-//         assert_eq!(
-//             tpropvec.iter_temporal(1).collect::<Vec<_>>(),
-//             vec![(&1, Prop::Str("Pometry".into()))]
-//         );
-//         assert_eq!(
-//             tpropvec.iter_temporal(2).collect::<Vec<_>>(),
-//             vec![(&2, Prop::I32(2022))]
-//         );
-//         assert_eq!(
-//             tpropvec.iter_temporal(3).collect::<Vec<_>>(),
-//             vec![(&3, Prop::Str("Graph".into()))]
-//         );
-//     }
-//
-//     #[test]
-//     fn every_new_update_to_the_same_prop_is_recorded_as_history() {
-//         let mut tpropvec = LazyVec::from_temporal(1, 1, &Prop::Str("Pometry".into()));
-//         tpropvec.set_temporal(1, 2, &Prop::Str("Pometry Inc.".into()));
-//
-//         let prop1 = tpropvec.iter_temporal(1).collect::<Vec<_>>();
-//         assert_eq!(
-//             prop1,
-//             vec![
-//                 (&1, Prop::Str("Pometry".into())),
-//                 (&2, Prop::Str("Pometry Inc.".into()))
-//             ]
-//         );
-//     }
-//
-//     #[test]
-//     fn new_update_with_the_same_time_to_a_prop_is_ignored() {
-//         let mut tpropvec = LazyVec::from_temporal(1, 1, &Prop::Str("Pometry".into()));
-//         tpropvec.set_temporal(1, 1, &Prop::Str("Pometry Inc.".into()));
-//
-//         let prop1 = tpropvec.iter_temporal(1).collect::<Vec<_>>();
-//         assert_eq!(prop1, vec![(&1, Prop::Str("Pometry".into()))]);
-//     }
-//
-//     #[test]
-//     fn updates_to_every_prop_can_be_iterated() {
-//         let tpropvec = LazyVec::default();
-//
-//         assert_eq!(tpropvec.iter_temporal(1).collect::<Vec<_>>(), vec![]);
-//
-//         let mut tpropvec = LazyVec::from_temporal(1, 1, &Prop::Str("Pometry".into()));
-//         tpropvec.set_temporal(1, 2, &Prop::Str("Pometry Inc.".into()));
-//         tpropvec.set_temporal(2, 3, &Prop::I32(2022));
-//         tpropvec.set_temporal(3, 4, &Prop::Str("Graph".into()));
-//         tpropvec.set_temporal(3, 5, &Prop::Str("Graph Analytics".into()));
-//
-//         let prop1 = tpropvec.iter_temporal(1).collect::<Vec<_>>();
-//         assert_eq!(
-//             prop1,
-//             vec![
-//                 (&1, Prop::Str("Pometry".into())),
-//                 (&2, Prop::Str("Pometry Inc.".into()))
-//             ]
-//         );
-//
-//         let prop3 = tpropvec.iter_temporal(3).collect::<Vec<_>>();
-//         assert_eq!(
-//             prop3,
-//             vec![
-//                 (&4, Prop::Str("Graph".into())),
-//                 (&5, Prop::Str("Graph Analytics".into()))
-//             ]
-//         );
-//     }
-//
-//     #[test]
-//     fn updates_to_every_prop_can_be_window_iterated() {
-//         let tpropvec = LazyVec::default();
-//
-//         assert_eq!(
-//             tpropvec
-//                 .iter_window(1, i64::MIN..i64::MAX)
-//                 .collect::<Vec<_>>(),
-//             vec![]
-//         );
-//
-//         let mut tpropvec = LazyVec::from_temporal(1, 1, &Prop::Str("Pometry".into()));
-//         tpropvec.set_temporal(1, 2, &Prop::Str("Pometry Inc.".into()));
-//         tpropvec.set_temporal(2, 3, &Prop::I32(2022));
-//         tpropvec.set_temporal(3, 4, &Prop::Str("Graph".into()));
-//         tpropvec.set_temporal(3, 5, &Prop::Str("Graph Analytics".into()));
-//         tpropvec.set_temporal(2, 1, &Prop::I32(2021));
-//         tpropvec.set_temporal(2, 4, &Prop::I32(2023));
-//
-//         let prop1 = tpropvec.iter_window(1, 1..3).collect::<Vec<_>>();
-//         assert_eq!(
-//             prop1,
-//             vec![
-//                 (&1, Prop::Str("Pometry".into())),
-//                 (&2, Prop::Str("Pometry Inc.".into()))
-//             ]
-//         );
-//
-//         let prop3 = tpropvec.iter_window(3, 5..6).collect::<Vec<_>>();
-//         assert_eq!(prop3, vec![(&5, Prop::Str("Graph Analytics".into()))]);
-//
-//         assert_eq!(tpropvec.iter_window(2, 5..6).collect::<Vec<_>>(), vec![]);
-//
-//         assert_eq!(
-//             // Results are ordered by time
-//             tpropvec.iter_window(2, 1..i64::MAX).collect::<Vec<_>>(),
-//             vec![
-//                 (&1, Prop::I32(2021)),
-//                 (&3, Prop::I32(2022)),
-//                 (&4, Prop::I32(2023))
-//             ]
-//         );
-//
-//         assert_eq!(
-//             tpropvec.iter_window(2, 4..i64::MAX).collect::<Vec<_>>(),
-//             vec![(&4, Prop::I32(2023))]
-//         );
-//
-//         assert_eq!(
-//             tpropvec.iter_window(2, 2..i64::MAX).collect::<Vec<_>>(),
-//             vec![(&3, Prop::I32(2022)), (&4, Prop::I32(2023))]
-//         );
-//
-//         assert_eq!(
-//             tpropvec.iter_window(2, 5..i64::MAX).collect::<Vec<_>>(),
-//             vec![]
-//         );
-//
-//         assert_eq!(
-//             tpropvec.iter_window(2, i64::MIN..5).collect::<Vec<_>>(),
-//             // Results are ordered by time
-//             vec![
-//                 (&1, Prop::I32(2021)),
-//                 (&3, Prop::I32(2022)),
-//                 (&4, Prop::I32(2023))
-//             ]
-//         );
-//
-//         assert_eq!(
-//             tpropvec.iter_window(2, i64::MIN..1).collect::<Vec<_>>(),
-//             vec![]
-//         );
-//     }
-// }
+    #[test]
+    fn normal_operation() {
+        let mut vec = LazyVec::<u32>::Empty;
+
+        vec.set(5, 55);
+        vec.set(1, 11);
+        vec.set(8, 88);
+        assert_eq!(vec.get(5), Some(&55));
+        assert_eq!(vec.get(1), Some(&11));
+        assert_eq!(vec.get(0), Some(&0));
+        assert_eq!(vec.get(10), None); // FIXME: this should return the default, 0, as well, there is no need to return Option from get()
+
+        // FIXME: replace update_or_set() with update()
+        // the behavior should be the same for both cases, because we should be able to assume that
+        // any cell is prefilled with default values and can therefore be safely updated
+        vec.update_or_set(6, |n| { *n += 1 }, 66);
+        assert_eq!(vec.get(6), Some(&1));
+        vec.update_or_set(9, |n| { *n += 1 }, 99);
+        assert_eq!(vec.get(9), Some(&99));
+
+        assert_eq!(vec.filled_ids(), vec![1, 5, 6, 8, 9]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn set_fails_if_present() {
+        let mut vec = LazyVec::from(5, 55);
+        vec.set(5, 555);
+    }
+}
