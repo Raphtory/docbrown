2606d7f4481dbc7bbde05c602ce6338363fc5861
narnolddd <n.a.arnold@qmul.ac.uk>
2023-03-29 17:05:49 +0100

    placeholder algorithm for three node motifs

--- docbrown/db/src/algorithms.rs
+++ docbrown/db/src/algorithms.rs
@@ -5,3 +5,4 @@ pub mod local_clustering_coefficient;
 pub mod local_triangle_count;
 pub mod reciprocity;
 pub mod three_node_motifs;
+pub mod three_node_local;
---
+++ docbrown/db/src/algorithms/three_node_local.rs
@@ -0,0 +1,2 @@
+use crate::view_api::*;
+use crate::algorithms::three_node_motifs::*;
