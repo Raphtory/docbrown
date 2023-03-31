e6b886e0a91459dd98392f4421cd2fbe78eb75c8
Lucas Jeub <lucas.jeub@pometry.com>
2023-03-31 13:48:48 +0200

    fix format

--- python/src/graph_gen.rs
+++ python/src/graph_gen.rs
@@ -9,6 +9,10 @@ pub(crate) fn random_attachment(g: &PyGraph, vertices_to_add: usize, edges_per_s
 }
 
 #[pyfunction]
-pub(crate) fn ba_preferential_attachment(g: &PyGraph, vertices_to_add: usize, edges_per_step: usize) {
+pub(crate) fn ba_preferential_attachment(
+    g: &PyGraph,
+    vertices_to_add: usize,
+    edges_per_step: usize,
+) {
     pa(&g.graph, vertices_to_add, edges_per_step);
 }
