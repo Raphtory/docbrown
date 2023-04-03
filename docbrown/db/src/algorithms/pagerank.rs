8415c91b2c6936ed665fbf913ec4cb6bed02dd96
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2023-04-03 22:50:05 +0100

    fix default sum set to 1

--- docbrown/db/src/algorithms/pagerank.rs
+++ docbrown/db/src/algorithms/pagerank.rs
@@ -15,73 +15,138 @@ use rustc_hash::FxHashMap;
 use std::ops::{Add, AddAssign, Div, Mul, Range, Sub};
 
 #[derive(PartialEq, PartialOrd, Clone, Debug)]
-struct F32(f32);
+struct MulF32(f32);
 
-impl Zero for F32 {
+impl Zero for MulF32 {
     fn set_zero(&mut self) {
         *self = Zero::zero();
     }
 
     fn zero() -> Self {
-        F32(1.0f32)
+        MulF32(1.0f32)
     }
 
     fn is_zero(&self) -> bool {
-        *self == F32(1.0f32)
+        *self == MulF32(1.0f32)
     }
 }
 
-impl Add for F32 {
-    type Output = F32;
+impl Add for MulF32 {
+    type Output = MulF32;
 
     fn add(self, rhs: Self) -> Self::Output {
-        F32(self.0 + rhs.0)
+        MulF32(self.0 + rhs.0)
     }
 }
 
-impl AddAssign for F32 {
+impl AddAssign for MulF32 {
     fn add_assign(&mut self, rhs: Self) {
         self.0 = self.0 + rhs.0
     }
 }
 
-impl Sub for F32 {
-    type Output = F32;
+impl Sub for MulF32 {
+    type Output = MulF32;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        F32(self.0 - rhs.0)
+        MulF32(self.0 - rhs.0)
     }
 }
 
-impl Div for F32 {
-    type Output = F32;
+impl Div for MulF32 {
+    type Output = MulF32;
 
     fn div(self, rhs: Self) -> Self::Output {
-        F32(self.0 / rhs.0)
+        MulF32(self.0 / rhs.0)
     }
 }
 
-impl Mul for F32 {
-    type Output = F32;
+impl Mul for MulF32 {
+    type Output = MulF32;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        F32(self.0 * rhs.0)
+        MulF32(self.0 * rhs.0)
     }
 }
 
-impl Bounded for F32 {
+impl Bounded for MulF32 {
     fn min_value() -> Self {
-        F32(f32::MIN)
+        MulF32(f32::MIN)
     }
 
     fn max_value() -> Self {
-        F32(f32::MAX)
+        MulF32(f32::MAX)
+    }
+}
+
+#[derive(PartialEq, PartialOrd, Clone, Debug)]
+struct SumF32(f32);
+
+impl Zero for SumF32 {
+    fn set_zero(&mut self) {
+        *self = Zero::zero();
+    }
+
+    fn zero() -> Self {
+        SumF32(0.0f32)
+    }
+
+    fn is_zero(&self) -> bool {
+        *self == SumF32(1.0f32)
+    }
+}
+
+impl Add for SumF32 {
+    type Output = SumF32;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        SumF32(self.0 + rhs.0)
+    }
+}
+
+impl AddAssign for SumF32 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0 = self.0 + rhs.0
+    }
+}
+
+impl Sub for SumF32 {
+    type Output = SumF32;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        SumF32(self.0 - rhs.0)
+    }
+}
+
+impl Div for SumF32 {
+    type Output = SumF32;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        SumF32(self.0 / rhs.0)
+    }
+}
+
+impl Mul for SumF32 {
+    type Output = SumF32;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        SumF32(self.0 * rhs.0)
+    }
+}
+
+impl Bounded for SumF32 {
+    fn min_value() -> Self {
+        SumF32(f32::MIN)
+    }
+
+    fn max_value() -> Self {
+        SumF32(f32::MAX)
     }
 }
 
 struct UnweightedPageRankS1 {
-    score: AccId<F32, F32, F32, ValDef<F32>>,
-    recv_score: AccId<F32, F32, F32, SumDef<F32>>,
+    score: AccId<MulF32, MulF32, MulF32, ValDef<MulF32>>,
+    recv_score: AccId<SumF32, SumF32, SumF32, SumDef<SumF32>>,
 }
 
 impl UnweightedPageRankS1 {
@@ -97,24 +162,24 @@ impl Program for UnweightedPageRankS1 {
     type Out = ();
 
     fn local_eval(&self, c: &LocalState) {
-        let score: AggRef<F32, F32, F32, ValDef<F32>> = c.agg(self.score.clone());
-        let recv_score: AggRef<F32, F32, F32, SumDef<F32>> = c.agg(self.recv_score.clone());
+        let score: AggRef<MulF32, MulF32, MulF32, ValDef<MulF32>> = c.agg(self.score.clone());
+        let recv_score: AggRef<SumF32, SumF32, SumF32, SumDef<SumF32>> = c.agg(self.recv_score.clone());
 
         c.step(|s| {
             let out_degree = s.out_degree().unwrap();
             let new_score = s.read(&score).0;
-            let w = F32(new_score / out_degree as f32);
+            let w = new_score / out_degree as f32;
             if out_degree > 0 {
                 for t in s.neighbours() {
-                    t.update(&recv_score, w.clone())
+                    t.update(&recv_score, SumF32(w))
                 }
             }
         });
     }
 
     fn post_eval(&self, c: &mut GlobalEvalState) {
-        let _ = c.agg(val::<F32>(0));
-        let _ = c.agg(sum::<F32>(1));
+        let _ = c.agg(val::<MulF32>(0));
+        let _ = c.agg(sum::<SumF32>(1));
         c.step(|_| true)
     }
 
@@ -126,9 +191,9 @@ impl Program for UnweightedPageRankS1 {
 }
 
 struct UnweightedPageRankS2 {
-    score: AccId<F32, F32, F32, ValDef<F32>>,
-    recv_score: AccId<F32, F32, F32, SumDef<F32>>,
-    max_diff: AccId<F32, F32, F32, MaxDef<F32>>,
+    score: AccId<MulF32, MulF32, MulF32, ValDef<MulF32>>,
+    recv_score: AccId<SumF32, SumF32, SumF32, SumDef<SumF32>>,
+    max_diff: AccId<MulF32, MulF32, MulF32, MaxDef<MulF32>>,
 }
 
 impl UnweightedPageRankS2 {
@@ -145,22 +210,23 @@ impl Program for UnweightedPageRankS2 {
     type Out = ();
 
     fn local_eval(&self, c: &LocalState) {
-        let score: AggRef<F32, F32, F32, ValDef<F32>> = c.agg(self.score.clone());
-        let recv_score: AggRef<F32, F32, F32, SumDef<F32>> = c.agg(self.recv_score.clone());
-        let max_diff: AggRef<F32, F32, F32, MaxDef<F32>> = c.global_agg(self.max_diff.clone());
+        let score: AggRef<MulF32, MulF32, MulF32, ValDef<MulF32>> = c.agg(self.score.clone());
+        let recv_score: AggRef<SumF32, SumF32, SumF32, SumDef<SumF32>> = c.agg(self.recv_score.clone());
+        let max_diff: AggRef<MulF32, MulF32, MulF32, MaxDef<MulF32>> = c.global_agg(self.max_diff.clone());
 
         c.step(|s| {
-            s.update(&score, s.read(&recv_score));
+            let r = s.read(&recv_score);
+            s.update(&score, MulF32(r.0));
             let prev = s.read_prev(&score);
             let curr = s.read(&score);
-            s.global_update(&max_diff, F32(abs((prev - curr).0)));
+            s.global_update(&max_diff, MulF32(abs((prev - curr).0)));
         });
     }
 
     fn post_eval(&self, c: &mut GlobalEvalState) {
-        let _ = c.agg(val::<F32>(0));
-        let _ = c.agg(sum::<F32>(1));
-        let _ = c.global_agg(max::<F32>(2));
+        let _ = c.agg(val::<MulF32>(0));
+        let _ = c.agg(sum::<SumF32>(1));
+        let _ = c.global_agg(max::<MulF32>(2));
         c.step(|_| true)
     }
 
@@ -172,8 +238,8 @@ impl Program for UnweightedPageRankS2 {
 }
 
 struct UnweightedPageRankS3 {
-    recv_score: AccId<F32, F32, F32, SumDef<F32>>,
-    max_diff: AccId<F32, F32, F32, MaxDef<F32>>,
+    recv_score: AccId<SumF32, SumF32, SumF32, SumDef<SumF32>>,
+    max_diff: AccId<MulF32, MulF32, MulF32, MaxDef<MulF32>>,
 }
 
 impl UnweightedPageRankS3 {
@@ -189,8 +255,8 @@ impl Program for UnweightedPageRankS3 {
     type Out = ();
 
     fn local_eval(&self, c: &LocalState) {
-        let recv_score: AggRef<F32, F32, F32, SumDef<F32>> = c.agg(self.recv_score.clone());
-        let max_diff: AggRef<F32, F32, F32, MaxDef<F32>> = c.global_agg(self.max_diff.clone());
+        let recv_score: AggRef<SumF32, SumF32, SumF32, SumDef<SumF32>> = c.agg(self.recv_score.clone());
+        let max_diff: AggRef<MulF32, MulF32, MulF32, MaxDef<MulF32>> = c.global_agg(self.max_diff.clone());
 
         c.step(|s| {
             s.reset(&recv_score);
@@ -199,8 +265,8 @@ impl Program for UnweightedPageRankS3 {
     }
 
     fn post_eval(&self, c: &mut GlobalEvalState) {
-        let _ = c.agg(sum::<F32>(1));
-        let _ = c.global_agg(max::<F32>(2));
+        let _ = c.agg(sum::<SumF32>(1));
+        let _ = c.global_agg(max::<MulF32>(2));
         c.step(|_| true)
     }
 
@@ -221,7 +287,7 @@ pub fn unweighted_page_rank(
     let pg_s2 = UnweightedPageRankS2::new();
     let pg_s3 = UnweightedPageRankS3::new();
 
-    let max_diff = F32(0.01f32);
+    let max_diff = MulF32(0.01f32);
     let mut i = 0;
 
     loop {
@@ -232,7 +298,7 @@ pub fn unweighted_page_rank(
         }
         i += 1;
 
-        if c.read_global_state(&max::<F32>(2)).unwrap() >= max_diff && i < iter_count {
+        if c.read_global_state(&max::<MulF32>(2)).unwrap() >= max_diff && i < iter_count {
             // if c.ss >= 3 && i < iter_count {
             break;
         }
@@ -245,7 +311,7 @@ pub fn unweighted_page_rank(
     (0..g.nr_shards)
         .into_iter()
         .fold(&mut results, |res, part_id| {
-            c.fold_state(&val::<F32>(0), part_id, res, |res, v_id, sc| {
+            c.fold_state(&val::<MulF32>(0), part_id, res, |res, v_id, sc| {
                 res.insert(*v_id, sc.0);
                 res
             })
