e6747c7e80ec872af22c56e5686652d21442d152
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2023-04-11 16:32:31 +0100

    fix bench and examples files

--- examples/rust/src/bin/lotr/main.rs
+++ examples/rust/src/bin/lotr/main.rs
@@ -1,9 +1,10 @@
 use docbrown::algorithms::triangle_count::{TriangleCountS1, TriangleCountS2, TriangleCountSlowS2};
 use docbrown::core::Prop;
 use docbrown::core::{state, utils};
+use docbrown::db::csv_loader::CsvLoader;
+use docbrown::db::graph::Graph;
 use docbrown::db::program::{GlobalEvalState, Program};
 use docbrown::db::view_api::*;
-use docbrown::db::{csv_loader::csv::CsvLoader, graph::Graph};
 use serde::Deserialize;
 use std::path::PathBuf;
 use std::{env, path::Path, time::Instant};
