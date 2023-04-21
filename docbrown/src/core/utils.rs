a87d368c14493d26a29bac6d171e04d8caa29060
Fabian Murariu <murariu.fabian@gmail.com>
2023-04-21 15:56:17 +0100

    make a specific EvalVertexView for task.rs

--- docbrown/src/core/utils.rs
+++ docbrown/src/core/utils.rs
@@ -10,6 +10,7 @@ pub fn calculate_hash<T: Hash>(t: &T) -> u64 {
     s.finish()
 }
 
+#[inline]
 pub fn get_shard_id_from_global_vid<T: Hash>(v_id: T, n_shards: usize) -> usize {
     let v_hash: u64 = calculate_hash(&v_id);
     let v: usize = v_hash.try_into().unwrap();
